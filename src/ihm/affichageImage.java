43ec4fb26d7fd5278fb307fa8608d8c9547aab15
saf <eudes.chatin@gmail.com>
2020-02-02 13:18:48 +0100

    resolution ant 2

--- src/ihm/affichageImage.java
+++ src/ihm/affichageImage.java
@@ -24,47 +24,65 @@ public class affichageImage extends Application {
 	@Override
 	
 	public void start(Stage premierStage) {
-		premierStage.setTitle("Ma première application JavaFX");
+		premierStage.setTitle("Ma premiere application JavaFX");
 		
 		
      
-		/*
-		 * 
-		 * 
-		 * Button _1 = new Button("Haut"); Button _2 = new Button("gauche"); Button _3 =
-		 * new Button("centre"); Button _4 = new Button("droite"); Button _5 = new
-		 * Button("bas"); Button _11 = new Button("Haut"); Button _21 = new
-		 * Button("gauche"); Button _31 = new Button("centre"); Button _41 = new
-		 * Button("droite"); Button _51 = new Button("bas");
-		 * 
-		 * BorderPane pane=new BorderPane();
-		 * 
-		 * _3.setMaxHeight(500); _3.setMaxWidth(500);
-		 * 
-		 * 
-		 * _1.setMaxWidth(500); _11.setMaxWidth(500); _5.setMaxWidth(500);
-		 * _51.setMaxWidth(500);
-		 * 
-		 * _2.setMaxHeight(500); _21.setMaxHeight(500); _4.setMaxHeight(500);
-		 * _41.setMaxHeight(500);
-		 * 
-		 * pane.setTop(_1); pane.setLeft(_2); pane.setCenter(_3); pane.setBottom(_5);
-		 * pane.setRight(_4);
-		 * 
-		 * BorderPane p2=new BorderPane(); p2.setTop(_11); p2.setLeft(_21);
-		 * p2.setCenter(pane); p2.setBottom(_51); p2.setRight(_41);
-		 * 
-		 * 
-		 * Button btnHello = new Button("Bonjour à tous"); //root.setCenter(btnHello);
-		 * Scene scene = new Scene(p2, 250, 100); premierStage.setScene(scene);
-		 * premierStage.initStyle(StageStyle.DECORATED);
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * premierStage.show();
-		 */
+         
+         
+		
+		Button _1 = new Button("Haut");
+		Button _2 = new Button("gauche");
+		Button _3 = new Button("centre");
+		Button _4 = new Button("droite");
+		Button _5 = new Button("bas");
+		Button _11 = new Button("Haut");
+		Button _21 = new Button("gauche");
+		Button _31 = new Button("centre");
+		Button _41 = new Button("droite");
+		Button _51 = new Button("bas");
+		
+		BorderPane pane=new BorderPane();
+		
+		_3.setMaxHeight(500);
+		_3.setMaxWidth(500);
+		
+		
+		_1.setMaxWidth(500);
+		_11.setMaxWidth(500);
+		_5.setMaxWidth(500);
+		_51.setMaxWidth(500);
+		
+		_2.setMaxHeight(500);
+		_21.setMaxHeight(500);
+		_4.setMaxHeight(500);
+		_41.setMaxHeight(500);
+		
+		pane.setTop(_1);
+		pane.setLeft(_2);
+		pane.setCenter(_3);
+		pane.setBottom(_5);
+		pane.setRight(_4);
+	
+		BorderPane p2=new BorderPane();
+		p2.setTop(_11);
+		p2.setLeft(_21);
+		p2.setCenter(pane);
+		p2.setBottom(_51);
+		p2.setRight(_41);
+		
+         
+		Button btnHello = new Button("Bonjour a tous");
+		//root.setCenter(btnHello);
+		Scene scene = new Scene(p2, 250, 100);
+		premierStage.setScene(scene);
+		premierStage.initStyle(StageStyle.DECORATED);
+		
+		
+		
+		
+		
+		premierStage.show();
 	}
 
 
